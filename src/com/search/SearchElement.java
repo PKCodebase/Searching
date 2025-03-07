26bc9ebcca4bfc70618f37702df04d87f3d123e5
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-07 16:50:07 +0530

    Using Linear Search

---
+++ src/com/search/SearchElement.java
@@ -0,0 +1,33 @@
+package com.search;
+import java.util.Scanner;
+public class SearchElement {
+
+    public static int searchElement(int arr[],int num){
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]== num){
+               return i;
+            }
+        }
+        return -1;
+
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to be searched: ");
+        int num = sc.nextInt();
+        int result = searchElement(arr,num);
+        System.out.println(result);
+
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        }else{
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
+
+//using LinearSearch
